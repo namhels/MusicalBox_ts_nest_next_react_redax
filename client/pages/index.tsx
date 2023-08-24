a69f97bda6603c35007f8d6ed985e6ebeb602368
namhels <8.lsv.888@gmail.com>
2023-08-24 13:08:16 +0300

    add Navbar

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,31 @@
 import React from "react"
+import SpaIcon from '@mui/icons-material/Spa';
+import Navbar from "@/components/Navbar";
 
 const Index = () => {
   return (
-    <div>
-      Yahooo
-    </div>
+    <>
+      <Navbar/>
+      <div className="center">
+        <h1>Welcome
+          <SpaIcon color="primary" fontSize="large" />
+        </h1>
+        <h3>
+          here are the best tracks
+        </h3>
+      </div>
+      <style jsx>
+        {`
+          .center {
+            margin-top: 150px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+          }
+        `}
+      </style>
+    </>
   )
 }
 
